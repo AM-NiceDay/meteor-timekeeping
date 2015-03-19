09e47ce83b65fb6fc7915ec89b9216f99bf1f866
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-03-19 08:11:35 +0300

    Init commit. Create timetable

---
+++ client/templates/layout.js
@@ -0,0 +1,81 @@
+Template.layout.helpers({
+	days: {
+		records: [{ 
+				hour: 9, 
+				minute: 0,
+				tag: 'java', 
+				description: 'Spring'
+			}, {
+				hour: 11,
+				minute: 0,
+				tag: 'work',
+				description: 'Scrum meetion'
+			}, {
+				hour: 11,
+				minute: 10,
+				tag: 'break',
+				description: 'Short break'
+			}, {
+				hour: 11,
+				minute: 20,
+				tag: 'work',
+				description: 'Personalization'
+			}, {
+				hour: 12,
+				minute: 20,
+				tag: 'work',
+				description: 'Scrum meetion'
+			}, {
+				hour: 12,
+				minute: 55,
+				tag: 'java',
+				description: 'Spring'
+			}, {
+				hour: 13,
+				minute: 35,
+				tag: 'break',
+				description: 'Long break'
+			}, {
+				hour: 14,
+				minute: 20,
+				tag: 'java',
+				description: 'Spring'
+			}, {
+				hour: 14,
+				minute: 40,
+				tag: 'work',
+				description: 'Personalization'
+			}, {
+				hour: 15,
+				minute: 35,
+				tag: 'break',
+				description: 'Short break'
+			}, {
+				hour: 15,
+				minute: 40,
+				tag: 'java',
+				description: 'Spring'
+			}, {
+				hour: 16,
+				minute: 0,
+				tag: 'break',
+				description: 'Long break'
+			}, {
+				hour: 16,
+				minute: 25,
+				tag: 'java',
+				description: 'Spring'
+			}, {
+				hour: 17,
+				minute: 25,
+				tag: 'break',
+				description: 'Short break'
+			}, {
+				hour: 17,
+				minute: 40,
+				tag: 'work',
+				description: 'Personalization'
+			}
+		]
+	}
+});
