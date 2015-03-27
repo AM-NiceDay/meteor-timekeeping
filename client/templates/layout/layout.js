e5af67cf73bdfed5ff4afcf06dbad81a4b2cf3b5
Aliaksei Krasikau <Aliaksei_Krasikau@epam.com>
2015-03-27 16:10:20 +0300

    Substitute Foundation with Materialize

--- client/templates/layout/layout.js
+++ client/templates/layout/layout.js
@@ -1,5 +1,23 @@
+Template.layout.rendered = function() {
+	 $(".button-collapse").sideNav();
+	 $('.collapsible').collapsible({
+      accordion : false
+    });
+}
+
 Template.layout.helpers({
 	days: function() {
 		return Days.find().fetch();
 	}
 });
+
+Template.daysList.rendered = function() {
+	$(".dropdown-button").dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: true,
+      hover: false,
+      alignment: 'right',
+      belowOrigin: true
+    });
+}
--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
 
-	if (Days.find().count() === 0 || Days.findOne().date != moment().subtract(1, 'days').format('YYYY-MM-DD')) {
+	if (Days.find().count() === 0 || Days.findOne().date != moment().format('YYYY-MM-DD')) {
 		console.log('Adding start days');
 		
 		lodash.each(Days.find().fetch(), function(day) {
