a6620ccccd3404b8ee18598c7742a4950cd27997
Aliaksei Krasikau <Aliaksei_Krasikau@epam.com>
2015-04-13 10:24:44 +0300

    Add timer of the current task https://app.asana.com/0/31138180314208/31428608887985

--- client/templates/time/record.js
+++ client/templates/time/record.js
@@ -5,16 +5,22 @@ Template.registerHelper('currentRecordTime', function() {
 	timer.tick();
 	var date = Session.get('date');
 	var day = Days.find({ date: date }).fetch()[0];
+	var now = moment();
 
-	if (day) {
-		var sortedRecords = lodash.sortBy(day.records, function(n) {
-	    return n.time;
+	if (day && date == now.format('YYYY-MM-DD') && !day.end) {
+		var filteredRecords = lodash.filter(day.records, function(record) {
+			return record.time < now.format('HH:mm');
+		});
+		var sortedRecords = lodash.sortBy(filteredRecords, function(record) {
+	    return record.time;
 		});
 
-		var lastRecord = sortedRecords[sortedRecords.length - 1];
-		var ms = moment().diff(moment(lastRecord.time, 'HH:mm'));
-		var d = moment.duration(ms);
-		var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
-		return s;
+		if (sortedRecords.length > 0) {
+			var lastRecord = sortedRecords[sortedRecords.length - 1];
+			var ms = now.diff(moment(lastRecord.time, 'HH:mm'));
+			var d = moment.duration(ms);
+			var s = Math.floor(d.asHours()) + moment.utc(ms).format(':mm:ss');
+			return s;
+		}
 	}
 });
